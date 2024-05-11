470605ee943fda7fdb07082e26bd780fb3d6c6d0
Tyler Fahey <twfahey1@gmail.com>
2024-05-10 21:33:36 -0500

    Add a functional builder form scaffolding that creates plugin blocks with Tailwind templates

---
+++ src/Form/BuilderForm.php
@@ -0,0 +1,180 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\aqto_ai_codegen\Form;
+
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Aqto AI Core form.
+ */
+final class BuilderForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'aqto_ai_codegen_builder';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['output'] = [
+      '#type' => 'markup',
+      '#markup' => '<div id="message-output"></div>',
+    ];
+    // A select list that allows picking one of the sites enabled modules.
+    $enabled_modules = \Drupal::moduleHandler()->getModuleList();
+    $form['module_fieldset'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Module'),
+    ];
+    $form['module_fieldset']['module'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Module'),
+      '#options' => array_combine(array_keys($enabled_modules), array_keys($enabled_modules)),
+      '#required' => TRUE,
+      '#prefix' => '<div id="module-output">',
+      '#suffix' => '</div>',
+    ];
+    // Lets add a "Refresh modules" that will invoke an ajax we'll write that refreshes the list
+    $form['module_fieldset']['refresh'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Refresh modules'),
+      '#ajax' => [
+        'callback' => '::refreshModulesAjax',
+        'wrapper' => 'module-output',
+      ],
+    ];
+
+    // Lets start a fieldgroup area for "Component Builder"
+    $form['component_builder'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Component Builder'),
+    ];
+
+    // A textfield for the component name.
+    $form['component_builder']['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+      '#required' => TRUE,
+    ];
+
+    // A textfield for the template name.
+    $form['component_builder']['template'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Template file name'),
+      '#description' => $this->t('The name of the theme and template file to use e.g. "my-component".'),
+      '#required' => TRUE,
+    ];
+
+    // A textarea to describe the data of the component.
+    $form['component_builder']['data'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Data'),
+      '#required' => TRUE,
+    ];
+
+    // A textarea to describe the appearance of the component.
+    $form['component_builder']['appearance'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Appearance'),
+      '#required' => TRUE,
+    ];
+
+    // Lets add an ajax callback submit now to do a upcoming function we'll write in a minute.
+    $form['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Build'),
+        '#ajax' => [
+          'callback' => '::buildComponentAjax',
+          'wrapper' => 'component-output',
+        ],
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * The refreshModulesAjax callback
+   */
+  public function refreshModulesAjax(array &$form, FormStateInterface $form_state): array {
+    // Lets get the enabled modules.
+    // Lets return the response.
+    return $form['module_fieldset']['module'];
+  }
+
+  /**
+   * AJAX callback to process the input message and interact with AI.
+   */
+  public function buildComponentAjax(array &$form, FormStateInterface $form_state): array {
+    // Lets get the form values.
+    $module = $form_state->getValue('module');
+    $name = $form_state->getValue('name');
+    $template = $form_state->getValue('template');
+    $data = $form_state->getValue('data');
+    $appearance = $form_state->getValue('appearance');
+
+    $siteActionsManager = \Drupal::service('aqto_ai_core.site_actions_manager');
+    $message = 'action: create_a_plugin_block_and_theme_and_template_aka_component, module ' . $module . ', name: ' . $name . ', template: ' . $template . ', data: ' . $data . ', appearance specs:' . $appearance;
+    $response = $siteActionsManager->invokeActionableQuestion($message);
+
+
+    // Lets create a response object.
+    $response = new AjaxResponse();
+    $response->addCommand(new HtmlCommand('#message-output', $this->t('Created component in the module @module with name @name, template @template, data @data, appearance @appearance', [
+      '@module' => $module,
+      '@name' => $name,
+      '@template' => $template,
+      '@data' => $data,
+      '@appearance' => $appearance,
+    ])));
+
+    // Lets build the component.
+    $component = [
+      'name' => $name,
+      'template' => $template,
+      'data' => $data,
+      'appearance' => $appearance,
+    ];
+
+    // Lets return the component as a render array.
+    return [
+      '#type' => 'markup',
+      '#markup' => '<pre>' . print_r($component, TRUE) . '</pre>',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    // @todo Validate the form here.
+    // Example:
+    // @code
+    //   if (mb_strlen($form_state->getValue('message')) < 10) {
+    //     $form_state->setErrorByName(
+    //       'message',
+    //       $this->t('Message should be at least 10 characters.'),
+    //     );
+    //   }
+    // @endcode
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    
+  }
+
+}
--- src/Generator.php
+++ src/Generator.php
@@ -9,19 +9,22 @@ use Drupal\aqto_ai_core\Utilities;
 /**
  * @todo Add class description.
  */
-final class Generator {
+final class Generator
+{
 
   /**
    * Constructs a Generator object.
    */
   public function __construct(
     private readonly Utilities $aqtoAiCoreUtilities,
-  ) {}
+  ) {
+  }
 
   /**
    * A method to generate a module given the name.
    */
-  public function generateModuleScaffold($module_name): void {
+  public function generateModuleScaffold($module_name): void
+  {
     // We want to take the module name and scaffold our files.
     // Let's use php to do this.
     // We will need to create a directory for the module.
@@ -35,10 +38,13 @@ final class Generator {
       "description: 'A module'\n" .
       "core_version_requirement: 10.x\n" .
       "package: 'Custom'\n";
-    
+
     file_put_contents($info_file, $info_file_contents);
     // We will need to create a .module file.
     $module_file = $module_path . '/' . $module_name . '.module';
+    \Drupal::service('module_installer')->install([
+      $module_name
+    ], TRUE);
   }
 
   /**
@@ -51,51 +57,76 @@ final class Generator {
    * - Add the function to the file.
    * - Save file and return true
    */
-  public function generateThemeFunction($module_name, $hook_name, $variables): bool {
+  public function generateThemeFunction($module_name, $hook_name, $variables): bool
+{
     $current_path = getcwd();
     $module_path = $current_path . '/modules/' . $module_name;
     $module_file = $module_path . '/' . $module_name . '.module';
 
     // Ensure the directory exists
-    if (!file_exists($module_path)) {
-        mkdir($module_path, 0777, true); // Make sure to handle permission correctly
+    if (!file_exists($module_path) && !mkdir($module_path, 0777, true)) {
+        return false; // Failed to create directory
     }
 
-    // Initialize the module file if it doesn't exist
+    // Initialize or read the module file
+    $file_needs_php_open_tag = false;
     if (!file_exists($module_file)) {
-        file_put_contents($module_file, "<?php\n\n");
+        $file_needs_php_open_tag = true;
+        $module_file_contents = "";
+    } else {
+        $module_file_contents = file_get_contents($module_file);
     }
 
-    $module_file_contents = file_get_contents($module_file);
-
-    // Check if the theme function already exists to avoid duplicate entries
-    if (!strpos($module_file_contents, "function {$module_name}_theme()")) {
-        $module_name_theme = $module_name . '_theme';
-        $hook_theme = "function $module_name_theme() {\n" .
-          "  return [\n" .
-          "    '$hook_name' => [\n" .
-          "      'variables' => [\n";
-
-        foreach ($variables as $variable) {
-            $hook_theme .= "        '$variable' => NULL,\n";
-        }
-
-        $hook_theme .= "      ],\n" .
-          "    ],\n" .
-          "  ];\n" .
-          "}\n";
-
-        $module_file_contents .= $hook_theme;
-        file_put_contents($module_file, $module_file_contents);
+    // Check if the theme function already exists
+    if (!preg_match('/function\s+' . $module_name . '_theme\s*\(\)\s*{\s*return\s*\[/', $module_file_contents)) {
+        $module_file_contents .= ($file_needs_php_open_tag ? "<?php\n\n" : "\n") . "function {$module_name}_theme() {\n  return [\n  ];\n}\n";
     }
 
-    return TRUE;
+    // Add new hook to theme function if it doesn't exist
+    if (strpos($module_file_contents, "'$hook_name' =>") === false) {
+        $new_hook_content = $this->buildThemeFunction($hook_name, $variables);
+        $module_file_contents = $this->addNewHookToThemeFunction($module_file_contents, $new_hook_content);
+    }
+
+    // Write the updated contents back to the module file
+    return file_put_contents($module_file, $module_file_contents) !== false;
 }
+
+private function addNewHookToThemeFunction($existing_contents, $new_hook_content): string
+{
+    $pattern = '/(\s*\])\s*;\s*\}\s*$/'; // Matches the closing parts of the theme function
+    $replacement = "    $new_hook_content,\n  $1;\n}"; // Correctly places the new hook and ensures the function closes properly
+    return preg_replace($pattern, $replacement, $existing_contents);
+}
+
+private function buildThemeFunction($hook_name, $variables): string
+{
+    return "'$hook_name' => [\n" .
+           "      'variables' => [\n" .
+           $this->formatVariables($variables) .
+           "      ]\n" .
+           "    ]";
+}
+
+private function formatVariables($variables): string
+{
+    $variables = array_unique($variables); // Ensure no duplicates
+    $variable_strings = [];
+    foreach ($variables as $variable_key => $variable_type) {
+        $variable_strings[] = "        '$variable_key' => NULL";
+    }
+    return implode(",\n", $variable_strings) . "\n";
+}
+
+
+
+
 
   /**
    * A public function to generate a new Paragraph entity with desired fields.
    */
-  public function generatePluginBlockAndThemeAndTemplate($module, $block_name, $block_primitive_values, $theme_name) {
+  public function generatePluginBlockAndThemeAndTemplate($module, $block_name, $variables, $theme_name)
+  {
     // the  $theme_template_name should be  $theme_template_name = $module . '-' . $block_name . '.html.twig'; with all _ replaced with slash
     $theme_template_name = str_replace('_', '-', $theme_name) . '.html.twig';
     /**
@@ -105,10 +136,9 @@ final class Generator {
      * - Next we'll scaffold the plugin block and write that data, we want to make sure to include the primitive types e.g. string, number, etc. fields from those arrays. It should map to our theme and implement a render array like #theme and all the necessary variables. 
      *  -Once we've written the plugin block, we'll create a new twig template that leverages TailwindCSS classes to make a presentation for all the primitive types. This should have already been referenced in the step where we implemented hook_theme()/
      */
-    $this->generateThemeFunction($module, $theme_name, $block_primitive_values);
-    $this->generatePluginBlock($module, $block_name, $block_primitive_values, $theme_name);
-    $this->generateTwigTemplate($module, $theme_name, $block_name,  $block_primitive_values, $theme_template_name);
-
+    $this->generateThemeFunction($module, $theme_name, $variables);
+    $this->generatePluginBlock($module, $block_name, $variables, $theme_name);
+    $this->generateTwigTemplate($module, $theme_name, $block_name,  $variables, $theme_template_name);
   }
 
   /**
@@ -123,96 +153,96 @@ final class Generator {
    */
   public function generatePluginBlock($module, $block_name, $block_primitive_values, $theme_name)
   {
-      // Create the directories
-      $current_path = getcwd();
-      // Lets make sure our plugin block file is properly cased like if the block_name is "Hero photo" we want it to be HeroPhoto.php
-      $block_name = str_replace(' ', '', ucwords($block_name));
-      $module_path = $current_path . '/modules/' . $module;
-      $block_path = $module_path . '/src/Plugin/Block';
-      if (!file_exists($block_path)) {
-        mkdir($block_path, 0777, TRUE);
-      }
-  
-      // Create the block file
-      $block_file = $block_path . '/' . $block_name . '.php';
-      $block_file_contents = "<?php\n\n";
-      $block_file_contents .= "namespace Drupal\\$module\\Plugin\\Block;\n\n";
-      $block_file_contents .= "use Drupal\\Core\\Block\\BlockBase;\n";
-      $block_file_contents .= "use Drupal\\Core\\Form\\FormStateInterface;\n";
-      $block_file_contents .= "use Symfony\\Component\\DependencyInjection\\ContainerInterface;\n";
-  
-      $block_file_contents .= "/**\n";
-      $block_file_contents .= " * Provides a '$block_name' block.\n";
-      $block_file_contents .= " *\n";
-      $block_file_contents .= " * @Block(\n";
-      $block_file_contents .= " *   id = \"$block_name\",\n";
-      $block_file_contents .= " *   admin_label = @Translation(\"$block_name\"),\n";
-      $block_file_contents .= " *   category = @Translation(\"$module\"),\n";
-      $block_file_contents .= " * )\n";
-      $block_file_contents .= " */\n";
-      $block_file_contents .= "class $block_name extends BlockBase {\n";
-  
-      // Define the block configuration
-      $block_file_contents .= "  /**\n";
-      $block_file_contents .= "   * {@inheritdoc}\n";
-      $block_file_contents .= "   */\n";
-      $block_file_contents .= "  public function defaultConfiguration() {\n";
-      $block_file_contents .= "    return [\n";
-      foreach ($block_primitive_values as $field_name => $type) {
-          $block_file_contents .= "      '$field_name' => '',\n";
-      }
-      $block_file_contents .= "    ] + parent::defaultConfiguration();\n";
-      $block_file_contents .= "  }\n";
-  
-      // Add block form for configuration
-      $block_file_contents .= "  /**\n";
-      $block_file_contents .= "   * {@inheritdoc}\n";
-      $block_file_contents .= "   */\n";
-      $block_file_contents .= "  public function blockForm(\$form, FormStateInterface \$form_state) {\n";
-      $block_file_contents .= "    \$form = parent::blockForm(\$form, \$form_state);\n";
-      $block_file_contents .= "    \$config = \$this->getConfiguration();\n";
-      foreach ($block_primitive_values as $field_name => $type) {
-          $block_file_contents .= "    \$form['$field_name'] = [\n";
-          $block_file_contents .= "      '#type' => '$type',\n";
-          $block_file_contents .= "      '#title' => t('" . ucfirst($field_name) . "'),\n";
-          $block_file_contents .= "      '#default_value' => isset(\$config['$field_name']) ? \$config['$field_name'] : '',\n";
-          $block_file_contents .= "    ];\n";
-      }
-      $block_file_contents .= "    return \$form;\n";
-      $block_file_contents .= "  }\n";
-  
-      // Save block configuration
-      $block_file_contents .= "  /**\n";
-      $block_file_contents .= "   * {@inheritdoc}\n";
-      $block_file_contents .= "   */\n";
-      $block_file_contents .= "  public function blockSubmit(\$form, FormStateInterface \$form_state) {\n";
-      $block_file_contents .= "    parent::blockSubmit(\$form, \$form_state);\n";
-      foreach ($block_primitive_values as $field_name => $type) {
-          $block_file_contents .= "    \$this->configuration['$field_name'] = \$form_state->getValue('$field_name');\n";
-      }
-      $block_file_contents .= "  }\n";
-  
-      // Build the block content
-      $block_file_contents .= "  /**\n";
-      $block_file_contents .= "   * {@inheritdoc}\n";
-      $block_file_contents .= "   */\n";
-      $block_file_contents .= "  public function build() {\n";
-      $block_file_contents .= "    \$config = \$this->getConfiguration();\n";
-      $block_file_contents .= "    return [\n";
-      $block_file_contents .= "      '#theme' => '$theme_name',\n";
-      foreach ($block_primitive_values as $field_name => $type) {
-          $block_file_contents .= "      '#$field_name' => \$config['$field_name'],\n";
-      }
+    // Create the directories
+    $current_path = getcwd();
+    // Lets make sure our plugin block file is properly cased like if the block_name is "Hero photo" we want it to be HeroPhoto.php
+    $block_name = str_replace(' ', '', ucwords($block_name));
+    $module_path = $current_path . '/modules/' . $module;
+    $block_path = $module_path . '/src/Plugin/Block';
+    if (!file_exists($block_path)) {
+      mkdir($block_path, 0777, TRUE);
+    }
+
+    // Create the block file
+    $block_file = $block_path . '/' . $block_name . '.php';
+    $block_file_contents = "<?php\n\n";
+    $block_file_contents .= "namespace Drupal\\$module\\Plugin\\Block;\n\n";
+    $block_file_contents .= "use Drupal\\Core\\Block\\BlockBase;\n";
+    $block_file_contents .= "use Drupal\\Core\\Form\\FormStateInterface;\n";
+    $block_file_contents .= "use Symfony\\Component\\DependencyInjection\\ContainerInterface;\n";
+
+    $block_file_contents .= "/**\n";
+    $block_file_contents .= " * Provides a '$block_name' block.\n";
+    $block_file_contents .= " *\n";
+    $block_file_contents .= " * @Block(\n";
+    $block_file_contents .= " *   id = \"$block_name\",\n";
+    $block_file_contents .= " *   admin_label = @Translation(\"$block_name\"),\n";
+    $block_file_contents .= " *   category = @Translation(\"$module\"),\n";
+    $block_file_contents .= " * )\n";
+    $block_file_contents .= " */\n";
+    $block_file_contents .= "class $block_name extends BlockBase {\n";
+
+    // Define the block configuration
+    $block_file_contents .= "  /**\n";
+    $block_file_contents .= "   * {@inheritdoc}\n";
+    $block_file_contents .= "   */\n";
+    $block_file_contents .= "  public function defaultConfiguration() {\n";
+    $block_file_contents .= "    return [\n";
+    foreach ($block_primitive_values as $field_name => $type) {
+      $block_file_contents .= "      '$field_name' => '',\n";
+    }
+    $block_file_contents .= "    ] + parent::defaultConfiguration();\n";
+    $block_file_contents .= "  }\n";
+
+    // Add block form for configuration
+    $block_file_contents .= "  /**\n";
+    $block_file_contents .= "   * {@inheritdoc}\n";
+    $block_file_contents .= "   */\n";
+    $block_file_contents .= "  public function blockForm(\$form, FormStateInterface \$form_state) {\n";
+    $block_file_contents .= "    \$form = parent::blockForm(\$form, \$form_state);\n";
+    $block_file_contents .= "    \$config = \$this->getConfiguration();\n";
+    foreach ($block_primitive_values as $field_name => $type) {
+      $block_file_contents .= "    \$form['$field_name'] = [\n";
+      $block_file_contents .= "      '#type' => '$type',\n";
+      $block_file_contents .= "      '#title' => t('" . ucfirst($field_name) . "'),\n";
+      $block_file_contents .= "      '#default_value' => isset(\$config['$field_name']) ? \$config['$field_name'] : '',\n";
       $block_file_contents .= "    ];\n";
-      $block_file_contents .= "  }\n";
-  
-      $block_file_contents .= "}\n";
-  
-      file_put_contents($block_file, $block_file_contents);
-  
-      return TRUE;
+    }
+    $block_file_contents .= "    return \$form;\n";
+    $block_file_contents .= "  }\n";
+
+    // Save block configuration
+    $block_file_contents .= "  /**\n";
+    $block_file_contents .= "   * {@inheritdoc}\n";
+    $block_file_contents .= "   */\n";
+    $block_file_contents .= "  public function blockSubmit(\$form, FormStateInterface \$form_state) {\n";
+    $block_file_contents .= "    parent::blockSubmit(\$form, \$form_state);\n";
+    foreach ($block_primitive_values as $field_name => $type) {
+      $block_file_contents .= "    \$this->configuration['$field_name'] = \$form_state->getValue('$field_name');\n";
+    }
+    $block_file_contents .= "  }\n";
+
+    // Build the block content
+    $block_file_contents .= "  /**\n";
+    $block_file_contents .= "   * {@inheritdoc}\n";
+    $block_file_contents .= "   */\n";
+    $block_file_contents .= "  public function build() {\n";
+    $block_file_contents .= "    \$config = \$this->getConfiguration();\n";
+    $block_file_contents .= "    return [\n";
+    $block_file_contents .= "      '#theme' => '$theme_name',\n";
+    foreach ($block_primitive_values as $field_name => $type) {
+      $block_file_contents .= "      '#$field_name' => \$config['$field_name'],\n";
+    }
+    $block_file_contents .= "    ];\n";
+    $block_file_contents .= "  }\n";
+
+    $block_file_contents .= "}\n";
+
+    file_put_contents($block_file, $block_file_contents);
+
+    return TRUE;
   }
-  
+
 
   /**
    * Same for twig template.
@@ -226,6 +256,11 @@ final class Generator {
    */
   public function generateTwigTemplate($module, $theme_name, $block_name,  $block_primitive_values, $theme_template_name)
   {
+    // Get the template getComponentAppearanceMarkupJson
+    $template_data = $this->getComponentAppearanceMarkupJson(json_encode($block_primitive_values), $theme_name);
+    $template_markup = $template_data['template_markup'];
+    $sample_data = $template_data['sample'];
+
     // Create the dir
     $current_path = getcwd();
     $module_path = $current_path . '/modules/' . $module;
@@ -235,17 +270,24 @@ final class Generator {
     }
     // Create the file
     $template_file = $template_path . '/' . $theme_template_name;
-    $template_file_contents = "<div class='grid grid-cols-2 gap-4'>\n";
-    foreach ($block_primitive_values as $key => $value) {
-      $template_file_contents .= "  <div class='bg-gray-100 p-4'>\n";
-      $template_file_contents .= "    <h2 class='text-lg font-bold'>$key</h2>\n";
-      $template_file_contents .= "    <p>$value</p>\n";
-      $template_file_contents .= "  </div>\n";
-    }
-    $template_file_contents .= "</div>\n";
+    $template_file_contents = $template_markup;
+
     file_put_contents($template_file, $template_file_contents);
     return TRUE;
   }
 
 
+  /**
+   * A callback to get an openAiResponse for the questino of "provide tailwind based html markup only that is for a block, the data for the block is $data, and then design ideas are $appearance".
+   * 
+   * It should return JSON with 'template_markup' which should be twig ready for block and 'sample' with key value pairs of sample data.
+   * 
+   */
+  public function getComponentAppearanceMarkupJson(string $data, string $appearance)
+  {
+    $question_for_openai = "We're designing a Tailwind based component, it will be used in our Drupal site with twig. RETURN ONLY JSON with key of 'template_markup' that contains a twig template, and then also a 'sample' which contains the relevant key and values of the data with a random fun sample values. For reference here is the base64 encoded data: " . base64_encode($data) . ", and here is the appearance requirements base64 encoded: " . base64_encode($appearance);
+    $component_data_json = \Drupal::service('aqto_ai_core.utilities')->getOpenAiJsonResponse($question_for_openai);
+    
+    return $component_data_json;
+  }
 }
